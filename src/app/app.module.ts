300b657251e289edaa5aa677446e325b3e29d4c3
Wandile <wandile1997@gmail.com>
2021-11-13 22:23:16 +0200

    Basic Sevices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatButtonModule,
     //MaterialModule,
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
